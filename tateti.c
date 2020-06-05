61122cc032a92a9fb5360b447812278fcde96369
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2020-06-05 05:29:08 -0300

    chequear ganador

--- tateti.c
+++ tateti.c
@@ -179,6 +179,77 @@ void intro_yo(char c[3][3]){
     c[i][j] = 'X';
 }
 
-int ganador(char c[3][3]){
-     
+int ganador (char c[3][3]){
+ if (c[0][0] == 'X' || c[0][0] == 'O'){
+  if (c[0][0] == c[0][1] && c[0][0] == c[0][2]){
+   if (c[0][0] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+  if (c[0][0] == c[1][0] && c[0][0] == c[2][0]){
+   if (c[0][0] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+ }
+ 
+ if (c[1][1] == 'X' || c[1][1] == 'O'){
+  if (c[1][1] == c[0][0] && c[1][1] == c[2][2]){
+   if (c[1][1] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+  if(c[1][1] == c[1][0] && c[1][1] c[1][2]){
+    if (c[1][1] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+  if(c[1][1] == c[2][0] && c[1][1] c[0][2]){
+    if (c[1][1] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+  if(c[1][1] == c[0][1] && c[1][1] c[2][1]){
+    if (c[1][1] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+ }
+ if (c[2][2] == 'X' || c[2][2] =='O'){
+  if (c[2][2] == c[2][0] && c[2][2] ==c[2][1]){
+    if (c[2][2] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+  if (c[2][2] == c[0][2] && c[2][2] == c[1][2]){
+    if (c[2][2] =='X'){
+    return 0; //He ganado
+   }
+   else {
+    return 1;//He perdido
+   }
+  }
+ }
+ return 2;
 }
